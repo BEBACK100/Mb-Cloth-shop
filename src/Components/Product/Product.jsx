d12d081e6eef9e655249b610103a9b1958fd3ee3
DELL-PC <beback_bain@yahoo.com>
2023-09-26 12:48:14 +0600

    youtube and facebook link add

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -8,6 +8,7 @@ const Product = () => {
     return (
         <div>
             <h1>Product is the part:{data.length}</h1>
+            <h1 className='text-center text-5xl text-green-600 font-bold m-5'>"You Can Choice your best Product"</h1>
             <div className=' grid grid-cols-4  gap-4 ms-4 mb-5'>
                 {
                     data.map(siproduct=><SingalProduct
--- src/Components/Product/ProductDetails.jsx
+++ src/Components/Product/ProductDetails.jsx
@@ -5,11 +5,13 @@ const ProductDetails = () => {
     const product=useLoaderData()
     console.log(product)
     return (
-        <div>
-            <p className='text-blue-600 text-4xl'><Link to="/product">Go to Product page</Link></p>
+        <div className='flex justify-center m-6'>
+           <div>
+           <p className='text-blue-600 text-4xl'><Link to="/product">Go to Product page</Link></p>
             <h1>This is Your Choiceable Product Details</h1>
             <h1>Product Title Name:<strong>{product.title} </strong></h1>
             <img src={product.thumbnailUrl} alt="" />
+           </div>
         </div>
     );
 };
